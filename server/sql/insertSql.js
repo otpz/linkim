299c23c0be068a1b4b5831aa9981e745f8af154c
Osman Topuz <osmantopuz98@gmail.com>
2024-03-06 22:56:53 +0300

    added regex to validation form

--- server/sql/insertSql.js
+++ server/sql/insertSql.js
@@ -4,7 +4,6 @@ const insertUser = async (name, surname, username, email, password) => {
     const query = `insert into Users (Email, Password, UserName, Name, Surname, JoinDate) values ('${email}', '${password}', '${username}', '${name}', '${surname}', GETDATE())`
     try {
         const result = await sql.query(query)
-        console.log(result)
         return result
     } catch (error) {
         console.log(error)
